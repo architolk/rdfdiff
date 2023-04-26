71e171e8e69945f3f9556acba373f0d60ed650a0
architolk <msj@xs4all.nl>
2023-04-26 11:49:51 +0200

    First working version

---
+++ rdfdiff.sh
@@ -0,0 +1,1 @@
+java -jar target/rdfdiff-*-jar-with-dependencies.jar data/file1.ttl data/file2.ttl data/output
---
+++ src/main/java/nl/architolk/rdfdiff/Compare.java
@@ -0,0 +1,78 @@
+package nl.architolk.rdfdiff;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.update.UpdateExecution;
+import org.apache.jena.update.UpdateExecutionFactory;
+import org.apache.jena.update.UpdateFactory;
+import org.apache.jena.update.UpdateRequest;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.riot.RDFLanguages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Compare {
+
+  static final String QUERY_UNIQUE1 = "INSERT { GRAPH <urn:unique1> {?s?p?o}} WHERE {GRAPH <urn:model1> {?s?p?o} FILTER NOT EXISTS {GRAPH <urn:model2> {?s?p?o}}}";
+  static final String QUERY_UNIQUE2 = "INSERT { GRAPH <urn:unique2> {?s?p?o}} WHERE {GRAPH <urn:model2> {?s?p?o} FILTER NOT EXISTS {GRAPH <urn:model1> {?s?p?o}}}";
+
+  private static final Logger LOG = LoggerFactory.getLogger(Compare.class);
+
+  public static void main(String[] args) {
+
+    if (args.length == 3) {
+
+      LOG.info("Starting conversion");
+      LOG.info("File #1: {}",args[0]);
+      LOG.info("File #2: {}",args[1]);
+      LOG.info("Output: {}",args[2]);
+
+      try {
+        Model firstModel = RDFDataMgr.loadModel(args[0]);
+        Model secondModel = RDFDataMgr.loadModel(args[1]);
+
+        Dataset dataset = DatasetFactory.create();
+        dataset.addNamedModel("urn:model1",firstModel);
+        dataset.addNamedModel("urn:model2",secondModel);
+
+        try {
+          LOG.info("Find unique values in model #1 (not in model #2)");
+          UpdateRequest request1 = UpdateFactory.create(QUERY_UNIQUE1);
+          UpdateExecution.dataset(dataset).update(request1).execute();
+
+          LOG.info("Find unique values in model #2 (not in model #1)");
+          UpdateRequest request2 = UpdateFactory.create(QUERY_UNIQUE2);
+          UpdateExecution.dataset(dataset).update(request2).execute();
+
+          dataset.commit();
+        } finally {
+          dataset.end();
+        }
+
+        Model outModel1 = dataset.getNamedModel("urn:unique1");
+        outModel1.setNsPrefixes(firstModel);
+        RDFDataMgr.write(new FileOutputStream(args[2]+"_1.ttl"),outModel1, RDFLanguages.filenameToLang(args[0],RDFLanguages.JSONLD));
+
+        Model outModel2 = dataset.getNamedModel("urn:unique2");
+        outModel2.setNsPrefixes(secondModel);
+        RDFDataMgr.write(new FileOutputStream(args[2]+"_2.ttl"),outModel2, RDFLanguages.filenameToLang(args[0],RDFLanguages.JSONLD));
+
+        LOG.info("Done!");
+      }
+      catch (Exception e) {
+        LOG.error(e.getMessage(),e);
+      }
+    } else {
+      LOG.info("Usage: rdfdiff <first.ttl> <second.ttl> <output>");
+    }
+  }
+}
